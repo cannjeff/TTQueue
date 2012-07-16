34677e7f46caae7f05bf493e17934fca771ddf67
Jeffrey Cann <cannjeff@gmail.com>
2012-07-16 19:18:28 -0400

    First commit

---
+++ ttQueue.js
@@ -0,0 +1,15 @@
+/*
+ *	TTQueue, Queue management for turntable.fm
+ *
+ *	Written by: Jeff Cann (@cannjeff)
+ */
+
+(function(window) {
+	window.ttQueue = {
+		init: function() {
+			console.log('run');
+		}
+	}
+})(window);
+
+ttQueue.init();
